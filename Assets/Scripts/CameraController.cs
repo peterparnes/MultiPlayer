04adc647544db79000664682c1d950c1d64db15f
Peter Parnes <peter@parnes.com>
2016-07-14 22:02:07 +0200

    game up and running

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+	GameObject target;
+	Vector3 offset;
+
+	public void MyInit(GameObject player)
+	{
+		target = player;
+		// offset = transform.position;
+		offset = new Vector3(0.0f,0.5f,0.0f);
+	}
+
+	void Start ()
+	{
+	}
+		
+	void LateUpdate ()
+	{
+		if (target) {
+			transform.position = target.transform.position + offset;
+			transform.rotation = target.transform.rotation;
+		}
+	}
+}
